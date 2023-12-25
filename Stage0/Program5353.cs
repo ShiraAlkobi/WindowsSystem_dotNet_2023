be2a143826c60eb40ed597f805975d1e9e2d8540
Shira Alkobi <salkobi@g.jct.ac.il>
2023-12-25 18:34:23 +0200

    Initial Code+Welcome functions

---
+++ Stage0/Program5353.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5353();
+            Welcome7213();
+            Console.ReadKey();
+        }
+
+        private static void Welcome5353()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void Welcome7213();
+    }
+}
