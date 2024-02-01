b3545515d2403432c7ae05a7832bd7ba60561715
Shira Alkobi <salkobi@g.jct.ac.il>
2024-02-01 12:08:01 +0200

    documantation

--- BL/BlApi/IBl.cs
+++ BL/BlApi/IBl.cs
@@ -1,8 +1,11 @@
 ﻿namespace BlApi;
-
+/// <summary>
+/// this interface wrapps all of the BL head entities` interfaces
+/// using this one we can access the entities' interfaces 
+/// </summary>
 public interface IBl
 {
-    public ITask Task { get;}
+    public ITask Task { get;} 
     public IEngineer Engineer { get; }
 
 }
--- BL/BlImplementation/EngineerImplementation.cs
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -1,36 +1,65 @@
 ﻿namespace BlImplementation;
-using BlApi;
+//using BlApi;
 using BO;
 using System;
 using System.Collections.Generic;
+//using DalApi;
 
-internal class EngineerImplementation : IEngineer
+/// <summary>
+/// in this file we implement all of the BL head entity Engineer's functions 
+/// we defined them in the interface IEngineer 
+/// </summary>
+internal class EngineerImplementation : BlApi.IEngineer
 {
+    /// <summary>
+    /// this field connects us to the data source. 
+    /// the Factoy.Get function returns the current form of data (list or XML)
+    /// it is being kept in a IDal object
+    /// </summary>
     private DalApi.IDal _dal = DalApi.Factory.Get;
+
+    /// <summary>
+    /// this function gets a BO engineer oblect and adds it to the data source (converting from BO object to DO one)
+    /// the function returns the id of the engineer added
+    /// </summary>
+    /// <param name="t"> the gotten engineer object</param>
+    /// <returns></returns>
+    /// <exception cref="BO.BlInputCheckException"></exception>
+    /// <exception cref="BO.BlAlreadyExistsException"></exception>
     public int Create(BO.Engineer t)
     {
+        ///Check for correct input
         if (t.Id < 0 || t.Name == "" || t.Cost < 0 || t.Email == "" || !t.Email!.Contains('@'))
         {
             throw new BO.BlInputCheckException("wrong input\n");
         }
+        ///creates the DO engineer using the right values from the gotten object
         DO.Engineer t_engineer = new DO.Engineer(t.Id, t.Email, t.Cost, t.Name, (DO.EngineerExperience)(int)t.Level);
-        try
+        try///the create function can throw an exeption
         {
-            int t_id = _dal.Engineer.Create(t_engineer);
+            ///calling for create to add the engineer to the data source
+            int t_id = _dal.Engineer.Create(t_engineer); 
             return t_id;
-        }
+        }///catching the exception that can be thrown, then throwing it to the PL level to handle
         catch (DO.DalAlreadyExistsException e) { throw new BO.BlAlreadyExistsException($"Engineer with ID={t.Id} already exists");/*TODO: להוסיף חריגה מתאימה*/}
     }
 
+    /// <summary>
+    /// deletes an engineer from the data source by id
+    /// </summary>
+    /// <param name="id">the engineer's id to delete</param>
+    /// <exception cref="BO.BlCanNotDelete"></exception>
+    /// <exception cref="BO.BlDoesNotExistException"></exception>
     public void Delete(int id)
     {
-        try
+        try 
         {
-            if (Read(id)!.Task == null)
+            ///search for the engineer, if found - check if there's no task assigned to it, then it can be deleted
+            if (Read(id)!.Task == null) 
                 _dal.Engineer.Delete(id);
-            else
+            else /// if there's a task assigned to the engineer - can't delete, throw an exception
                 throw new BO.BlCanNotDelete($"can`t delete engineer with ID={id}");
-        }
+        } ///catch any exception that can be thrown from the delete function
         catch(DO.DalDoesNotExistException e) { throw new BO.BlDoesNotExistException($"Engineer with ID={id} doesn`t exist"); }
     }
 
